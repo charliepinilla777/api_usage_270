6eaf0567f7478b15ea3e64febc14c618f21b0854
Carlos Andres Pinilla Castrillon <106884751+charliepinilla777@users.noreply.github.com>
2024-09-29 20:18:14 -0500

    css and componet complete all changes Api coffee hot

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import { ApiCoffeCarruselBasic } from './Componets/ApiCoffeeCarruselBasic'
+
+function App() {
+  
+  return (
+    <div>
+      <ApiCoffeCarruselBasic/>
+    </div>
+  )
+}
+
+export default App
